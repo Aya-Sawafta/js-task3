dc4a73ae89fec23504325c388373249359c4668d
AyaSawafta <sawaftaaya5@gmail.com>
2024-07-09 23:20:36 +0300

    create list using js

---
+++ main.js
@@ -0,0 +1,9 @@
+var list = "";
+lang1 = "C++";
+lang2 = "C#";
+lang3 = "Node-js"
+document.querySelector("body").innerHTML = `<ul>
+ <li>${lang1} </li>
+ <li>${lang2} </li>
+ <li>${lang3} </li>
+  </ul>`;
